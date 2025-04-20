98e0d5f92f5d9e8c743430ad8a888fba58543b69
gzchenny <gzchenny@gmail.com>
2025-04-20 21:14:08 +1000

    Added EmailJS and Supabase files

--- scripts/email.js
+++ scripts/email.js
@@ -1,12 +1,11 @@
 (function() {
-    emailjs.init("C-FJ34pNntexXgk6t"); // Replace with your EmailJS Public Key
+    emailjs.init("-gq8Ylj-DwOJwQpoc");
 })();
 
-// Add event listener to the form
 document.getElementById('enquiryForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    emailjs.sendForm('service_3tyjizo', 'template_f5pnqah', this)
+    emailjs.sendForm('service_5alcehl', 'template_w8i62qa', this)
         .then(() => {
             document.getElementById('successMessage').style.display = 'block';
             document.getElementById('errorMessage').style.display = 'none';
